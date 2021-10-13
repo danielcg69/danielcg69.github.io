4d0adf9615d1b685666c358e11b1e54fa260297f
danielcg69 <danielcg69@gmail.com>
2021-10-13 18:18:27 -0300

    con validacion

--- js/validsign.js
+++ js/validsign.js
@@ -3,7 +3,7 @@ function validpass(p){
     var passr=p.rptpassword.value
 
     if (pass != passr){
-    alert ("las claves son distintas, Verifique por favor")}
+    alert ("Las claves son distintas, Verifique por favor!")}
     else
-    {alert("CLAVES OK")}
+    {alert("Claves coinciden")}
 }
