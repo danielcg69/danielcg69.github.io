926fb4c4961ee3a1d2361663b6bfc84ea87b5d1a
danielcg69 <danielcg69@gmail.com>
2021-10-13 18:53:48 -0300

    Vlidacion traducida

--- js/app.js
+++ js/app.js
@@ -18,3 +18,13 @@
         }, false)
       })
   })()
+//valida que las dos password sean iguales
+  function validpass(p){
+    var pass=p.password.value
+    var passr=p.rptpassword.value
+
+    if (pass != passr){
+    alert ("Las claves son distintas, Verifique por favor!")}
+    else
+    {alert("Claves coinciden")}
+}
--- js/validsign.js
+++
@@ -1,9 +0,0 @@
-function validpass(p){
-    var pass=p.password.value
-    var passr=p.rptpassword.value
-
-    if (pass != passr){
-    alert ("Las claves son distintas, Verifique por favor!")}
-    else
-    {alert("Claves coinciden")}
-}
